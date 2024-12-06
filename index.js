6a6142972e7872b5788975c2723b721d981c590d
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-06 14:14:05 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// 1.saturdayFun
+function saturdayFun(activity = "roller-skate") { 
+        
+    console.log (`This Saturday, I am going to ${activity}!`);
+          
+}
+saturdayFun(("bathe my dog"));
+
+   
+
+// 2. mondayWork
+function mondayWork(task = "go to the office") {
+    return `This Monday, I need to ${task}.`;
+  }
+
+
+//3. wrapAdjective
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
